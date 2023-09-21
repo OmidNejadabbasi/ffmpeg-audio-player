3329b6c53bca1790b7f4eb5e84562ecee41a31e5
Omid Nejadabbasi <omidne@systemgroup.net>
2023-09-21 17:03:23 +0330

    init commit

---
+++ src/library.cpp
@@ -0,0 +1,11 @@
+#include "library.h"
+
+#include <iostream>
+
+void hello() {
+    std::cout << "Hello, World!" << std::endl;
+
+    int a = 8935+234;
+    int b = 4 + a;
+    std::cout << b;
+}
---
+++ src/library.h
@@ -0,0 +1,6 @@
+#ifndef FFMPEG_CODE_LIBRARY_H
+#define FFMPEG_CODE_LIBRARY_H
+
+void hello();
+
+#endif //FFMPEG_CODE_LIBRARY_H
